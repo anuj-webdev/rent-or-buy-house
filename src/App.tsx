4a69d19dfb92e5cf1f47cd40b83f41e0dfbb9cca
Anuj Sharma <anujsharma.engg@gmail.com>
2022-05-04 22:07:18 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Base component
+ */
+
+const App = () => (
+    <div style={{ fontFamily: 'Segoe UI light' }}>
+        <h1>Welcome to Truly Minimal React TypeScript boilerplate.</h1>
+        <h4>
+            Please visit &nbsp;
+            <a
+                href="https://github.com/anuj-webdev/minimal-react-typescript-boilerplate"
+                target="_blank"
+            >
+                minimal-react-typescript-boilerplate
+            </a> &nbsp;
+            & hit star ★ if this boilerplate saved your time.
+        </h4>
+    </div>
+)
+
+export default App
